05386240de8b7f30847d2c769287991283d2c2ad
sebastianuskh <sebastianus.kurniawan@gmail.com>
2018-11-20 20:56:45 +0700

    initial commit

---
+++ src/main/kotlin/com/tresor/app/Main.kt
@@ -0,0 +1,19 @@
+package com.tresor.app
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, 3001) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
+}
